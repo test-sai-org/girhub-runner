5313b99ac84aecd500d2f03f2290b12cf80c28f3
skommana04 <skommana04@gmail.com>
2026-02-09 05:32:26 -0600

    a

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "saidevops.site"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z06083563MQUI4X3GPRCP"
+  description = "description"
+}
